6113b9165642ed07ad0a5cdee58a0548925875f5
scateu <scateu@gmail.com>
2018-07-08 22:42:12 +0800

    yay..

--- set-png-alpha-divided-by-2.sh
+++ set-png-alpha-divided-by-2.sh
@@ -1,2 +1,5 @@
-convert $1 -channel Alpha -evaluate Divide 2 $(basename $1 .png)-alpha-0.5.png
+#convert $1 -channel Alpha -evaluate Divide 2 $(basename $1 .png)-alpha-0.5.png
 
+#convert $1 -alpha set -channel Alpha -evaluate set 50% png32:$(basename $1 .png)-alpha-0.5.png
+
+convert $1 -alpha set -background none -channel A -evaluate multiply 0.5 +channel png32:$(basename $1 .png)-alpha-0.5.png
