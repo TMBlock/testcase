8ebdf1b134868213bebb844acfdf0954f915f687
scateu <scateu@gmail.com>
2018-07-29 13:35:37 +0800

    color-wheel

---
+++ scripts/color-wheel/filter-color-range-from-color-wheel.py
@@ -0,0 +1,19 @@
+from __future__ import division
+from PIL import Image
+#image = Image.open("color_wheel.webp")
+image = Image.open("01.png")
+
+
+pix = image.load()
+
+
+for i in range(image.size[0]):
+    for j in range(image.size[1]):
+#        print pix[i,j]
+        scale = 0.9
+        if 127*scale <= pix[i,j][0] <= 255 and 127*scale <= pix[i,j][1] <= 255 and 127*scale <= pix[i,j][2] <= 255:
+            pass
+        else:
+            pix[i,j]=(255,255,255)
+
+image.save("generated.jpg")
