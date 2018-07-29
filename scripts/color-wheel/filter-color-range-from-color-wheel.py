e88a3a3e5cfa2ea550261ecd491ff6d63ce4f8ab
scateu <scateu@gmail.com>
2018-07-29 19:08:17 +0800

    rgb wheel

--- scripts/color-wheel/filter-color-range-from-color-wheel.py
+++ scripts/color-wheel/filter-color-range-from-color-wheel.py
@@ -1,7 +1,8 @@
 from __future__ import division
 from PIL import Image
 #image = Image.open("color_wheel.webp")
-image = Image.open("01.png")
+#image = Image.open("01.png")
+image = Image.open("wheel-3-rgb.png")
 
 
 pix = image.load()
@@ -10,10 +11,10 @@ pix = image.load()
 for i in range(image.size[0]):
     for j in range(image.size[1]):
 #        print pix[i,j]
-        scale = 0.9
+        scale = 1.0
         if 127*scale <= pix[i,j][0] <= 255 and 127*scale <= pix[i,j][1] <= 255 and 127*scale <= pix[i,j][2] <= 255:
             pass
         else:
             pix[i,j]=(255,255,255)
 
-image.save("generated.jpg")
+image.save("generated.png")
