659cb46b6a95920ab708e16ca81990b2dd43b82b
scateu <scateu@gmail.com>
2018-07-29 14:55:42 +0800

    yuv420p

--- scripts/do.sh
+++ scripts/do.sh
@@ -1,3 +1,5 @@
 #LD_LIBRARY_PATH=$HOME/Develop/TMBlock/libtmblock/build/ ./ffmpeg_g -i $1 -i ~/Develop/TMBlock/testcase/AliSecurity/Alibaba-Security-alpha-0.5.png -filter_complex "[0:v][1:v] tmblock=x=1600:y=800:func=$2 [out]" -map "[out]" -t 10 -y $3
-LD_LIBRARY_PATH=$HOME/Develop/TMBlock/libtmblock/build/ ./ffmpeg_g -i $1 -i ~/Develop/TMBlock/testcase/AliSecurity/Alibaba-Security-alpha-0.5.png -filter_complex "[0:v][1:v] tmblock=x=1200:func=$2 [out]" -map "[out]" -t 10 -y $3
+
+LD_LIBRARY_PATH=$HOME/Develop/TMBlock/libtmblock/build/ ./ffmpeg_g -i $1 -i ~/Develop/TMBlock/testcase/AliSecurity/Alibaba-Security-alpha-0.5.png -filter_complex "[0:v][1:v] tmblock=x=1200:func=$2 [out]" -map "[out]" -t 10 -pix_fmt yuv420p -y $3
+
 #LD_LIBRARY_PATH=$HOME/Develop/TMBlock/libtmblock/build/ ./ffmpeg_g -i $1 -i ~/Develop/TMBlock/testcase/AliSecurity/Alibaba-Security-alpha-0.5.png -filter_complex "[0:v][1:v] tmblock=x=100:func=$2 [out]" -map "[out]" -t 10 -y $3
